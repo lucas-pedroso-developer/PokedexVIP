40f4928492db4935daf7a47bc9843b2f761371cd
Lucas Pedroso <lucaspedroso@brq.com>
2023-04-16 08:28:37 -0300

    criando repository e endpoint

---
+++ PokedexVIP/Product/Data/PokedexEndpoint.swift
@@ -0,0 +1,7 @@
+import Foundation
+import Foundation
+
+struct PokedexEndpoint: NetworkRequest {
+    var method: HTTPMethod = .get
+}
+
---
+++ PokedexVIP/Product/Data/PokedexRepository.swift
@@ -0,0 +1,47 @@
+import Foundation
+
+public protocol PokedexRepositoryProtocol {
+    func fetchData(completion: @escaping (Result<[Pokemons], NetworkError>) -> Void)
+}
+
+public final class PokedexRepository {
+    
+    let network: NetworkManagerProtocol
+    
+    private let group: DispatchGroup = DispatchGroup()
+    private var pokemonsData: [Pokemons]?
+    
+    public init(network: NetworkManagerProtocol) {
+        self.network = network
+    }
+    
+    private func fetchHomeDataSource() {
+        group.enter()
+        network.request(PokedexEndpoint()) { [weak self] (response: Result<[Pokemons], Error>) in
+            switch response {
+            case let .success(data):
+                self?.pokemonsData = data
+                fallthrough
+            default:
+                self?.group.leave()
+            }
+        }
+    }
+}
+
+extension PokedexRepository: PokedexRepositoryProtocol {
+    
+    public func fetchData(completion: @escaping (Result<[Pokemons], NetworkError>) -> Void) {
+        fetchHomeDataSource()
+        group.notify(queue: .main) { [weak self] in
+            guard let pokemonsDataSource = self?.pokemonsData else {
+                completion(.failure(.noData))
+                return
+            }
+            let viewModel = pokemonsDataSource
+            completion(.success(viewModel))
+        }
+    }
+    
+    
+}
