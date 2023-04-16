6d7c2471e8e5f88031727059743529e440d4a1ec
Lucas Pedroso <lucaspedroso@brq.com>
2023-04-16 14:42:00 -0300

    criando a logica para iniciar a HomeViewController, criando a label de titulo

--- PokedexVIP/Product/Presentation/HomeViewController.swift
+++ PokedexVIP/Product/Presentation/HomeViewController.swift
@@ -5,6 +5,19 @@ class HomeViewController: UIViewController {
     
     public override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        setupLabel()
+    }
+    
+    private func setupLabel() {
+        let label = UILabel()
+        label.text = "Pokedex"
+        label.textAlignment = .center
+        view.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     }
     
 }
